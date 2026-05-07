08716556a077ed51acf84cdc4b691b65e9225b43
reyan <reyan@nazneens-MacBook-Air.local>
2026-05-07 12:16:01 +0530

    Use python directly with dynamic port

--- app.py
+++ app.py
@@ -112,4 +112,4 @@ def analyze():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+   app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
